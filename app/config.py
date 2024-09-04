a5f01ebbffc1ed31d115cedc5bd023213de48a05
Anaximandr228 <alex09.g29@gmail.com>
2024-09-04 23:46:04 +0500

    First commit

---
+++ app/config.py
@@ -0,0 +1,4 @@
+host = "localhost"  # Хост приложения
+user = "postgres"  # Имя пользователя PostgreSQL
+password = "rtf558Ur"  # Пароль пользователя PostgreSQL
+db_name = "test_work"  # Название базы данных PostgeSQL
