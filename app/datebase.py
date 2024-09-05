30e37b569bcc2a407b1a130ec26d1a7e33ef0f00
Anaximandr228 <alex09.g29@gmail.com>
2024-09-05 13:52:03 +0500

    Refactor code

--- app/datebase.py
+++ app/datebase.py
@@ -3,55 +3,74 @@ import psycopg2
 from config import host, user, password, db_name
 from timestamp_serializer import DatetimeEncoder
 
-try:
-    # Подключение к базе данных
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name
+
+# Создание таблицы
+def create_table(cursor):
+    cursor.execute(
+        """CREATE TABLE users(
+            id serial PRIMARY KEY,
+            first_name CHARACTER VARYING(30) NOT NULL,
+            last_name CHARACTER VARYING(30) NOT NULL,
+            date timestamp,
+            price REAL NOT NULL,
+            amount INTEGER);"""
     )
-    connection.autocommit = True
+    print("[INFO] Таблица создана")
 
-    # Создание таблицы
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE users(
-                id serial PRIMARY KEY,
-                first_name CHARACTER VARYING(30) NOT NULL,
-                last_name CHARACTER VARYING(30) NOT NULL,
-                date timestamp,
-                price REAL NOT NULL,
-                amount INTEGER);"""
+
+# Добавление данных
+def create_data(cursor):
+    cursor.execute(
+        """INSERT INTO users (first_name, last_name, date, price, amount) VALUES
+        ('Alex', 'Popkov', NOW(), '135.86', '34'),
+        ('Egor', 'Sidorov', NOW(), '15.85', '12');"""
+    )
+    print("[INFO] Данные были добавлены")
+
+
+# Получение данных
+def select_data(cursor):
+    cursor.execute(
+        """SELECT * FROM users;"""
+    )
+    print("[INFO] Данные были получены")
+
+
+# Создание JSON файла
+def json_create(cursor):
+    cursor.execute("""SELECT * FROM users;""")
+    user_list = cursor.fetchall()
+    user_list_json = json.dumps(user_list, cls=DatetimeEncoder)
+    with open("user_list.json", "w") as file:
+        file.write(user_list_json)
+    print("[INFO] JSON файл создан")
+
+
+def main():
+    try:
+        # Подключение к базе данных
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
         )
-    # Добавление данных
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """INSERT INTO users (first_name, last_name, date, price, amount) VALUES
-            ('Alex', 'Popkov', NOW(), '135.86', '34'),
-            ('Egor', 'Sidorov', NOW(), '15.85', '12');"""
-        )
+        connection.autocommit = True
 
-        print("[INFO] Data was succefully inserted")
+        # Создание таблицы
+        with connection.cursor() as cursor:
+            create_table(cursor)
+            create_data(cursor)
+            select_data(cursor)
+            json_create(cursor)
 
-    # Получение данных
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """SELECT * FROM users;"""
-        )
-
-    # Создание JSON файла
-    with connection.cursor() as cursor:
-        cursor.execute("""SELECT * FROM users;""")
-        user_list = cursor.fetchall()
-        user_list_json = json.dumps(user_list, cls=DatetimeEncoder)
-        with open("user_list.json", "w") as file:
-            file.write(user_list_json)
+    except Exception as _ex:
+        print("[INFO] Ошибка при работе с PostgreSQL", _ex)
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] PostgreSQL соединение закрыто")
 
 
-except Exception as _ex:
-    print("[INFO] Error while working with PostgreSQL", _ex)
-finally:
-    if connection:
-        connection.close()
-        print("[INFO] PostgreSQL connection closed")
+if __name__ == "__main__":
+    main()
